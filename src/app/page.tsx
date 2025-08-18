907bffae433ac40eeb15e01ed0242fd89d195b81
Nikhil <nikhildevs.108@gmail.com>
2025-08-18 16:09:26 -0700

    login card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use server";
 import { signIn, auth } from "@/auth";
-import { SignInButton } from "@/components/sign-in";
 import Link from "next/link";
 import Image from "next/image";
+import { LoginCard } from "@/components/login-card";
 
 export default async function Home() {
   const session = await auth();
@@ -15,9 +15,8 @@ export default async function Home() {
   }
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <p> You're not signed in! </p>
-      <SignInButton />
+    <div className="justify-items-center p-8 flex-col">
+      <LoginCard />
     </div>
   );
 }
--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -3,5 +3,10 @@ import { Button } from "@/components/ui/button";
 import { login } from "@/lib/actions/auth";
 
 export const SignInButton = () => {
-  return <Button onClick={() => login()}> Sign In with Github </Button>;
+  return (
+    <Button variant="outline" className="w-full" onClick={() => login()}>
+      {" "}
+      Sign In with Github{" "}
+    </Button>
+  );
 };
