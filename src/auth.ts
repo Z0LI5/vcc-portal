cd9e54f5c8c2e27d5f75f1f8f6d9540eb7e38ca4
Nikhil <nikhildevs.108@gmail.com>
2025-08-20 17:06:09 -0700

    firebase init

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,10 @@
+export const runtime = "nodejs";
+
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { firestore } from "./lib/firestore";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [GitHub, Google],
